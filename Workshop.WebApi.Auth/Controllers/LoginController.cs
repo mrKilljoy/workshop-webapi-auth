a3d45995fa7e005304a13e941de8d17370502705
Konstantin <sauron-cs@mail.ru>
2025-02-12 14:40:00 +0100

    added controller

---
+++ Workshop.WebApi.Auth/Controllers/LoginController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Workshop.WebApi.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
