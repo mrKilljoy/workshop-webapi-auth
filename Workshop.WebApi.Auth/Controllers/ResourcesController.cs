545547b1c6f558f2e4c6db72979a32e52e4fab22
Konstantin <sauron-cs@mail.ru>
2025-02-12 16:25:32 +0100

    added Resources controller

---
+++ Workshop.WebApi.Auth/Controllers/ResourcesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Workshop.WebApi.Auth.Infrastructure;
+
+namespace Workshop.WebApi.Auth.Controllers;
+
+[Route("api/[controller]")]
+public class ResourcesController : ControllerBase
+{
+    [HttpGet("public")]
+    public async Task<IActionResult> GetPublicResource()
+    {
+        return Ok("Here is the public resource");
+    }
+
+    [Authorize(AuthenticationSchemes = Constants.Authentication.Cookie)]
+    [HttpGet("protected")]
+    public async Task<IActionResult> GetProtectedResource()
+    {
+        return Ok("Here is the protected resource");
+    }
+}
