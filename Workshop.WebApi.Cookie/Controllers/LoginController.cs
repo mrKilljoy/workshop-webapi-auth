7929b4306d2a7e3d63b261c220a296a987d32fa6
Konstantin <sauron-cs@mail.ru>
2025-02-19 17:14:05 +0100

    extended Login API for working with refresh tokens

--- Workshop.WebApi.Cookie/Controllers/LoginController.cs
+++ Workshop.WebApi.Cookie/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Workshop.Shared.Services;
 using Workshop.WebApi.Cookie.Infrastructure;
 using Workshop.WebApi.Cookie.Infrastructure.Authentication;
+using Workshop.WebApi.Cookie.Infrastructure.Exceptions;
 using Workshop.WebApi.Cookie.Models;
 
 namespace Workshop.WebApi.Cookie.Controllers
@@ -13,12 +15,17 @@ namespace Workshop.WebApi.Cookie.Controllers
     {
         private readonly IUserManager _userManager;
         private readonly ITokenGenerator _tokenGenerator;
+        private readonly ITokenManager _tokenManager;
         private readonly IDataProtector _dataProtector;
 
-        public LoginController(IDataProtectionProvider idp, IUserManager userManager, ITokenGenerator tokenGenerator)
+        public LoginController(IDataProtectionProvider idp,
+            IUserManager userManager,
+            ITokenGenerator tokenGenerator,
+            ITokenManager tokenManager)
         {
             _userManager = userManager;
             _tokenGenerator = tokenGenerator;
+            _tokenManager = tokenManager;
             _dataProtector = idp.CreateProtector(Constants.Authentication.DataProtectorName);
         }
         
@@ -45,9 +52,29 @@ namespace Workshop.WebApi.Cookie.Controllers
                 return Unauthorized();
             }
 
-            var token = _tokenGenerator.Generate(request);
+            var result = new TokenPairModel()
+            {
+                AccessToken = CreateAccessToken(request.Login),
+                RefreshToken = await CreateRefreshToken(request.Login)
+            };
 
-            return Ok(token);
+            return Ok(result);
+        }
+
+        [Authorize(AuthenticationSchemes = Constants.Authentication.JwtSchemaName)]
+        [HttpPost("jwt/refresh")]
+        public async Task<IActionResult> JwtRefresh([FromBody] RefreshTokenModel request)
+        {
+            if (!await ValidateRefreshToken(request.RefreshToken))
+                return BadRequest("Invalid token");
+
+            var result = new TokenPairModel()
+            {
+                AccessToken = CreateAccessToken(GetUserLogin()),
+                RefreshToken = await CreateRefreshToken(GetUserLogin())
+            };
+            
+            return Ok(result);
         }
 
         private async Task<bool> ValidateLogin(CredentialsModel model)
@@ -65,5 +92,34 @@ namespace Workshop.WebApi.Cookie.Controllers
             
             Response.Cookies.Append(Constants.Authentication.CookieSchemaName, encryptedCookie);
         }
+
+        private string CreateAccessToken(string login) => _tokenGenerator.Generate(login);
+
+        private async Task<string> CreateRefreshToken(string login)
+        {
+            var user  = await _userManager.Get(login);
+            if (user is null)
+                throw new UserNotFoundException(login);
+
+            var item = await _tokenManager.GetToken(user.Id);
+            if (item is null)
+            {
+                var token = await _tokenManager.CreateToken(user.Id);
+                return token;
+            }
+
+            if (!item.IsValid())
+            {
+                await _tokenManager.RemoveToken(user.Id);
+                var token = await _tokenManager.CreateToken(user.Id);
+                return token;
+            }
+
+            return item.Value;
+        }
+
+        private async Task<bool> ValidateRefreshToken(string token) => await _tokenManager.IsValid(token);
+
+        private string? GetUserLogin() => User.Identity?.Name;
     }
 }
