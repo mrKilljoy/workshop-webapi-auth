5a1bea2f49efc79824bab42f02d8dcc148383560
Konstantin <sauron-cs@mail.ru>
2025-02-19 20:31:43 +0100

    minor model improvements

--- Workshop.WebApi.Authentication/Models/CredentialsModel.cs
+++ Workshop.WebApi.Authentication/Models/CredentialsModel.cs
@@ -1,8 +1,15 @@
-﻿namespace Workshop.WebApi.Authentication.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Workshop.WebApi.Authentication.Models;
 
 public class CredentialsModel
 {
+    [Required]
+    [JsonProperty("login", Required = Required.Always)]
     public string Login { get; set; }
 
+    [Required]
+    [JsonProperty("password", Required = Required.Always)]
     public string Password { get; set; }
 }
--- Workshop.WebApi.Authentication/Models/RefreshTokenModel.cs
+++ Workshop.WebApi.Authentication/Models/RefreshTokenModel.cs
@@ -1,9 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace Workshop.WebApi.Authentication.Models;
 
 public class RefreshTokenModel
 {
+    [Required]
     [JsonProperty("refreshToken", Required = Required.Always)]
     public string RefreshToken { get; set; }
 }
