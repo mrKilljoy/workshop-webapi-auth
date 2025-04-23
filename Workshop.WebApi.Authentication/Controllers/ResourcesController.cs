881200353c9326eb84a2d1e4505058fa2a45e81b
Konstantin <sauron-cs@mail.ru>
2025-04-23 16:32:13 +0200

    updated Swagger attributes on Resources endpoints

--- Workshop.WebApi.Authentication/Controllers/ResourcesController.cs
+++ Workshop.WebApi.Authentication/Controllers/ResourcesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Workshop.WebApi.Authentication.Infrastructure;
+using Workshop.WebApi.Authentication.Models;
 
 namespace Workshop.WebApi.Authentication.Controllers;
 
@@ -18,10 +19,10 @@ public class ResourcesController : ControllerBase
     /// <response code="200">Access granted.</response>
     [AllowAnonymous]
     [HttpGet("public")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResourceResponse), StatusCodes.Status200OK, "application/json")]
     public Task<IActionResult> GetPublicResource()
     {
-        return Task.FromResult<IActionResult>(Ok("Here is the public resource"));
+        return Task.FromResult<IActionResult>(Ok(BuildResponse("public", "Here is the public resource")));
     }
 
     /// <summary>
@@ -31,10 +32,12 @@ public class ResourcesController : ControllerBase
     /// <response code="200">Access granted.</response>
     /// <response code="401">Access denied.</response>
     [Authorize(AuthenticationSchemes = Constants.Authentication.CookieSchemaName)]
+    [ProducesResponseType(typeof(ResourceResponse), StatusCodes.Status200OK, "application/json")]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized, "application/json")]
     [HttpGet("protected-1")]
     public Task<IActionResult> GetProtectedResourceOne()
     {
-        return Task.FromResult((IActionResult)Ok("Here is the protected resource #1"));
+        return Task.FromResult((IActionResult)Ok(BuildResponse("protected-1", "Here is the protected resource #1")));
     }
     
     /// <summary>
@@ -44,10 +47,12 @@ public class ResourcesController : ControllerBase
     /// <response code="200">Access granted.</response>
     /// <response code="401">Access denied.</response>
     [Authorize(AuthenticationSchemes = Constants.Authentication.JwtSchemaName, Policy = Constants.Authentication.PolicyName)]
+    [ProducesResponseType(typeof(ResourceResponse), StatusCodes.Status200OK, "application/json")]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized, "application/json")]
     [HttpGet("protected-2")]
     public Task<IActionResult> GetProtectedResourceTwo()
     {
-        return Task.FromResult((IActionResult)Ok("Here is the protected resource #2"));
+        return Task.FromResult((IActionResult)Ok(BuildResponse("protected-2", "Here is the protected resource #2")));
     }
     
     /// <summary>
@@ -57,9 +62,14 @@ public class ResourcesController : ControllerBase
     /// <response code="200">Access granted.</response>
     /// <response code="401">Access denied.</response>
     [Authorize(AuthenticationSchemes = $"{Constants.Authentication.CookieSchemaName},{Constants.Authentication.JwtSchemaName}")]
+    [ProducesResponseType(typeof(ResourceResponse), StatusCodes.Status200OK, "application/json")]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized, "application/json")]
     [HttpGet("protected-3")]
     public Task<IActionResult> GetProtectedResourceThree()
     {
-        return Task.FromResult((IActionResult)Ok("Here is the protected resource #3"));
+        return Task.FromResult((IActionResult)Ok(BuildResponse("protected-3", "Here is the protected resource #3")));
     }
+
+    private ResourceResponse BuildResponse(string name, string content, int httpCode = 200)
+        => new() { Name = name, Content = content, HttpCode = httpCode};
 }
